fb3dec2472c76878845dea831bc136a3b9ea45b9
agarwp0 <agarwp0@sewanee.edu>
2022-05-02 14:26:17 +0200

    app v2

--- app.py
+++ app.py
@@ -2,74 +2,32 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-
-st.set_page_config(page_title = 'Streamlit Dashboard', 
+st.set_page_config(page_title = 'SDG Dashboard', 
     layout='wide',
-    page_icon='💹')
+    page_icon='')
 
 ### top row 
 
-st.markdown("## Main KPIs")
+hunger = pd.read_csv("./hunger-data.csv")
+test = hunger.T["World"][5]
+# test = 8.9
+hunger
 
-first_kpi, second_kpi, third_kpi = st.columns(3)
+st.markdown("## UN SDG")
 
-
-with first_kpi:
-    st.markdown("**First KPI**")
-    number1 = 111 
-    st.markdown(f"<h1 style='text-align: center; color: red;'>{number1}</h1>", unsafe_allow_html=True)
-
-with second_kpi:
-    st.markdown("**Second KPI**")
-    number2 = 222 
-    st.markdown(f"<h1 style='text-align: center; color: red;'>{number2}</h1>", unsafe_allow_html=True)
-
-with third_kpi:
-    st.markdown("**Third KPI**")
-    number3 = 333 
-    st.markdown(f"<h1 style='text-align: center; color: red;'>{number3}</h1>", unsafe_allow_html=True)
-
-
-### second row 
-
-st.markdown("<hr/>", unsafe_allow_html=True)
-
-st.markdown("## Secondary KPIs")
-
-first_kpi, second_kpi, third_kpi, fourth_kpi, fifth_kpi, sixth_kpi = st.columns(6)
+first_kpi, second_kpi= st.columns(2)
 
 
 with first_kpi:
-    st.markdown("**First KPI**")
-    number1 = 111 
+    st.markdown("**Zero hunger**")
+    number1 = test 
     st.markdown(f"<h1 style='text-align: center; color: red;'>{number1}</h1>", unsafe_allow_html=True)
+    st.markdown("*Measured by Undernourishment")
 
 with second_kpi:
-    st.markdown("**Second KPI**")
-    number2 = 222 
+    st.markdown("**Poverty**")
+    number2 = test 
     st.markdown(f"<h1 style='text-align: center; color: red;'>{number2}</h1>", unsafe_allow_html=True)
-
-with third_kpi:
-    st.markdown("**Third KPI**")
-    number3 = 333 
-    st.markdown(f"<h1 style='text-align: center; color: red;'>{number3}</h1>", unsafe_allow_html=True)
-
-with fourth_kpi:
-    st.markdown("**First KPI**")
-    number1 = 111 
-    st.markdown(f"<h1 style='text-align: center; color: red;'>{number1}</h1>", unsafe_allow_html=True)
-
-with fifth_kpi:
-    st.markdown("**Second KPI**")
-    number2 = 222 
-    st.markdown(f"<h1 style='text-align: center; color: red;'>{number2}</h1>", unsafe_allow_html=True)
-
-with sixth_kpi:
-    st.markdown("**Third KPI**")
-    number3 = 333 
-    st.markdown(f"<h1 style='text-align: center; color: red;'>{number3}</h1>", unsafe_allow_html=True)
-
-st.markdown("<hr/>", unsafe_allow_html=True)
 
 
 st.markdown("## Chart Section: 1")
